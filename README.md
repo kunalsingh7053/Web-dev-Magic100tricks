61808db3b0388959298b12e47391227d1fcdf4aa
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2025-04-30 14:48:41 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Web-dev-Magic100tricks
---
+++ Trick15/index.js
@@ -0,0 +1,21 @@
+let a = 10;
+let b = "10";
+
+if(a == b)
+{
+    console.log("Both are equal");
+}
+else{
+    console.log("Not Equal")
+
+}
+
+let c = 10;
+let d = "10";
+if(c === d)
+{
+    console.log("Both are equal");
+}
+else{
+    console.log("Not Equal")
+}
